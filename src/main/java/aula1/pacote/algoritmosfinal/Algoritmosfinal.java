ce958b91b8e036883a63aa1c5fbfe01e855f09a4
“Andrey” <neoandrey21@gmail.com>
2023-09-17 02:58:07 -0400

    primeiraAlteracao

---
+++ src/main/java/aula1/pacote/algoritmosfinal/Algoritmosfinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package aula1.pacote.algoritmosfinal;
+
+/**
+ *
+ * @author andre
+ */
+public class Algoritmosfinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
